3b8d4b966b505a8eb9d550277d2bbaf987dffca1
Aditya Singh <114297309+Adixya@users.noreply.github.com>
2024-01-09 15:29:08 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // 1. Summing all even numbers using a while loop
+    // int n, sum = 0, i = 0;
+
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+
+    // // Iterate through even numbers using a while loop
+    // while (i <= n)
+    // {
+    //     sum += i;
+    //     i += 2; // Increment i by 2 to get the next even number
+    // }
+
+    // cout << "The sum of even numbers up to " << n << " is: " << sum << endl;
+
+    // return 0;
+
+    // 2. Check no is prime or not using while loop
+    // int n, sum = 0, i = 2;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+
+    // while (i < n)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         cout << "Not prime for " << i << endl;
+    //     }
+    //     else
+    //     {
+    //         cout << "Prime for " << i << endl;
+    //     }
+    //     i += 1;
+    // }
+    // return 0;
+
+}

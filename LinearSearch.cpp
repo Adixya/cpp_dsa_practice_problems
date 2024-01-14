0954b1709f196f4a99ae500b5f4362bec8374e8d
Aditya Singh <114297309+Adixya@users.noreply.github.com>
2024-01-14 14:12:58 +0530

    Add files via upload

---
+++ LinearSearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+bool search(int arr[], int size, int key)
+{
+
+    for (int i = 0; i <= size; i++)
+    {
+        if (arr[i] == key)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+
+    int arr[10] = {5, -7, 10, -2, 2, 4, 9, 12, 22, -8};
+
+    cout << "Enter the element to search for: ";
+    int key;
+    cin >> key;
+
+    bool found = search(arr, 10, key);
+
+    if (found)
+    {
+        cout << "Key is Present";
+    }
+    else
+    {
+        cout << "Key is Absent";
+    }
+}

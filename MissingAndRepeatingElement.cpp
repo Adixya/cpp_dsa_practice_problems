6ba1275d99ee1d680f9113ada5b6fee31be6579c
Aditya Singh <114297309+Adixya@users.noreply.github.com>
2024-02-17 19:17:59 +0530

    Add files via upload

---
+++ MissingAndRepeatingElement.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> findTwoElement(vector<int> arr, int n)
+    {
+        // code here
+        vector<int> ans;
+        int rep = -1;
+        int mis = -1;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            mp[arr[i]]++;
+        }
+        for (auto it : mp)
+        {
+            if (it.second > 1)
+            {
+                rep = it.first;
+            }
+        }
+        int i = 1;
+        while (i <= n)
+        {
+            if (mp.find(i) == mp.end())
+            {
+                mis = i;
+                break;
+            }
+            i++;
+        }
+        ans.push_back(rep);
+        ans.push_back(mis);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

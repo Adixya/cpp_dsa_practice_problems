8246ab33cb4baed65163756e859296db5c08165e
Adixya - Aditya Singh <114297309+Adixya@users.noreply.github.com>
2024-07-29 20:43:21 +0530

    Add files via upload

--- CountInversions.cpp
+++ CountInversions.cpp
@@ -1,96 +1,92 @@
-//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution
-{
-public:
+class Solution {
+  public:
     // arr[]: Input Array
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
-    long long int inversionCount(long long arr[], long long N)
-    {
-        // Your Code Here
-        long long l = 0;
-        long long r = N - 1;
-        return countInv(arr, l, r);
-    }
-    long long countInv(long long arr[], long long l, long long r)
-    {
-        long long res = 0;
-        if (l < r)
-        {
-            long long m = l + (r - l) / 2; // Fix the typo here
-            res += countInv(arr, l, m);
-            res += countInv(arr, m + 1, r);
-            res += countandmerge(arr, l, m, r);
+    long long mergeAndCount(long long arr[], int start, int mid, int end) {
+    int len1 = mid - start + 1;
+    int len2 = end - mid;
+
+    // Create temporary arrays
+    long long* left = new long long[len1];
+    long long* right = new long long[len2];
+
+    // Copy data to temporary arrays
+    for (int i = 0; i < len1; i++)
+        left[i] = arr[start + i];
+    for (int i = 0; i < len2; i++)
+        right[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = start;
+    long long invCount = 0;
+
+    // Merge the two halves while counting inversions
+    while (i < len1 && j < len2) {
+        if (left[i] <= right[j]) {
+            arr[k++] = left[i++];
+        } else {
+            arr[k++] = right[j++];
+            invCount += (len1 - i); // All remaining elements in left[] are inversions
         }
-        return res;
     }
 
-    long long countandmerge(long long arr[], long long l, long long m, long long r)
-    {
-        long long n1 = m - l + 1;
-        long long n2 = r - m;
-        long long left[n1], right[n2];
-        long long res = 0; // Initialize res to 0
-        for (int i = 0; i < n1; i++)
-        {
-            left[i] = arr[l + i];
-        }
-        for (int i = 0; i < n2; i++)
-        {
-            right[i] = arr[m + 1 + i];
-        }
-        int i = 0, j = 0, k = l;
-        while (i < n1 && j < n2)
-        {
-            if (left[i] <= right[j]) // Fix the condition here
-            {
-                arr[k] = left[i];
-                i++;
-            }
-            else
-            {
-                arr[k] = right[j];
-                j++;
-                res = res + (n1 - i);
-            }
-            k++;
-        }
-        while (i < n1)
-        {
-            arr[k] = left[i];
-            i++;
-            k++;
-        }
-        while (j < n2)
-        {
-            arr[k] = right[j];
-            j++;
-            k++;
-        }
-        return res;
+    // Copy the remaining elements of left[], if there are any
+    while (i < len1) {
+        arr[k++] = left[i++];
     }
+
+    // Copy the remaining elements of right[], if there are any
+    while (j < len2) {
+        arr[k++] = right[j++];
+    }
+
+    // Deallocate memory
+    delete[] left;
+    delete[] right;
+
+    return invCount;
+}
+
+long long mergeSortAndCount(long long arr[], int start, int end) {
+    long long invCount = 0;
+    if (start < end) {
+        int mid = start + (end - start) / 2;
+
+        // Count inversions in the left half
+        invCount += mergeSortAndCount(arr, start, mid);
+
+        // Count inversions in the right half
+        invCount += mergeSortAndCount(arr, mid + 1, end);
+
+        // Count inversions during merge
+        invCount += mergeAndCount(arr, start, mid, end);
+    }
+    return invCount;
+}
+
+long long int inversionCount(long long arr[], int n) {
+    return mergeSortAndCount(arr, 0, n - 1);
+}
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
+int main() {
 
     long long T;
     cin >> T;
 
-    while (T--)
-    {
-        long long N;
+    while (T--) {
+        int N;
         cin >> N;
 
         long long A[N];
-        for (long long i = 0; i < N; i++)
-        {
+        for (long long i = 0; i < N; i++) {
             cin >> A[i];
         }
         Solution obj;
@@ -99,5 +95,3 @@ int main()
 
     return 0;
 }
-
-// } Driver Code Ends

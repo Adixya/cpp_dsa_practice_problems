243a1f65a3c841e59e23979cbfb83c0af8117eaf
Adixya - Aditya Singh <114297309+Adixya@users.noreply.github.com>
2024-08-02 22:35:12 +0530

    Add files via upload

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -1,51 +1,73 @@
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // } Driver Code Ends
-class Solution {
-  public:
-    int editDistance(string str1, string str2) {
-        int m = str1.size();
-        int n = str2.size();
-        
-        // Creating a 2D Vector DP 
-         vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-        
-        // Iterating through words
-        for(int i = 0; i <= m; i++){
-            for(int j = 0; j <= n; j++){
-                // If i == 0, i.e STR1 is empty, meaning it will require j number of insertions
-                if(i == 0){
-                    dp[i][j] = j;
-                }
-                
-                // If j == 0; i.e STR2 is empty, meaning it will require i number of deletions
-                else if(j == 0){
-                    dp[i][j] = i;
-                }
-                
-                // We got the same word, so we'll do nothing and take back the result of previous
-                else if(str1[i-1] == str2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                
-                // Word isn't matching so we'll take minimum of insertion, removal or replacement
-                else{
-                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
-                }
+class Solution
+{
+public:
+    int helper(vector<vector<int>> &dp, string a, string b, int i, int j)
+    {
+        // Base cases
+        if (i < 0)
+            return j + 1;
+        if (j < 0)
+            return i + 1;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (a[i] == b[j])
+            dp[i][j] = helper(dp, a, b, i - 1, j - 1);
+        else
+        {
+            dp[i][j] = 1 + min({helper(dp, a, b, i - 1, j), helper(dp, a, b, i, j - 1), helper(dp, a, b, i - 1, j - 1)});
+        }
+
+        return dp[i][j];
+    }
+
+    int editDistance(string str1, string str2)
+    {
+        // Code here
+        int n = str1.size(), m = str2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // Recursive solution
+        // return helper(dp, str1, str2, n - 1, m - 1);
+
+        // iterative solution
+        for (int i = 0; i <= n; i++)
+        {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= m; i++)
+        {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (str1[i - 1] == str2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
             }
         }
-        
-        return dp[m][n];
+
+        return dp[n][m];
     }
 };
 
 //{ Driver Code Starts.
-int main() {
+int main()
+{
     int T;
     cin >> T;
-    while (T--) {
+    while (T--)
+    {
         string s, t;
         cin >> s >> t;
         Solution ob;
@@ -55,3 +77,4 @@ int main() {
     return 0;
 }
 
+// } Driver Code Ends

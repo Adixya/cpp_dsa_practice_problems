7d252adf6993641687a55a5422ab71be3751eb6b
Adixya - Aditya Singh <114297309+Adixya@users.noreply.github.com>
2024-08-17 19:37:15 +0530

    Add files via upload

--- ProductArrayPuzzle.cpp
+++ ProductArrayPuzzle.cpp
@@ -1,86 +1,47 @@
-//{ Driver Code Starts
-// Initial template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
+
 
 // } Driver Code Ends
 // User function template for C++
 
-class Solution
-{
-public:
+class Solution {
+  public:
     // nums: given vector
     // return the Product vector P that hold product except self at each index
-    vector<long long int> productExceptSelf(vector<long long int> &nums, int n)
-    {
+    vector<long long int> productExceptSelf(vector<long long int>& nums) {
 
-        // code here
-        int count = 0;
-        long long product = 1;
-        long long product1 = 1;
-
-        for (int i = 0; i < n; i++)
-        {
-            if (nums[i] == 0)
-            {
-                count++;
+        long long int product = 1;
+        int zeroCnt = 0;
+        for(int i=0 ; i< nums.size() ; ++i){
+            if(nums[i]!=0){
+                product *= nums[i];
+            }else{
+                zeroCnt++;
             }
         }
-
-        for (int i = 0; i < n; i++)
-        {
-
-            if (count >= 2 || count == 0)
-            {
-                product = product * nums[i];
+        
+        vector<long long int> ans;
+        for(int i=0 ; i< nums.size() ; ++i){
+            if(nums[i]!=0){
+                if(zeroCnt>=1) ans.push_back(0);
+                else ans.push_back(product/(long long int)nums[i]);
+            }else{
+                if(zeroCnt>1) ans.push_back(0);
+                else ans.push_back(product);
             }
-            else
-            {
-                if (nums[i] != 0)
-                {
-                    product1 = product1 * nums[i];
-                }
-                else
-                {
-                    continue;
-                }
-            }
+            
         }
-
-        for (int i = 0; i < n; i++)
-        {
-            if (count == 0)
-            {
-                nums[i] = product / nums[i];
-            }
-            else if (count >= 2)
-            {
-                nums[i] = 0;
-            }
-            else
-            {
-                if (nums[i] != 0)
-                {
-                    nums[i] = 0;
-                }
-                else
-                {
-                    nums[i] = product1;
-                }
-            }
-        }
-        return nums;
+        return ans;
     }
 };
 
+
 //{ Driver Code Starts.
-int main()
-{
+int main() {
     int t; // number of test cases
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         int n; // size of the array
         cin >> n;
         vector<long long int> arr(n), vec(n);
@@ -90,7 +51,7 @@ int main()
             cin >> arr[i];
         }
         Solution obj;
-        vec = obj.productExceptSelf(arr, n); // function call
+        vec = obj.productExceptSelf(arr); // function call
 
         for (int i = 0; i < n; i++) // print the output
         {
@@ -100,4 +61,3 @@ int main()
     }
     return 0;
 }
-// } Driver Code Ends
